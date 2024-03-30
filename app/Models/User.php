f31047f185aafc6f1f9f929c33946c357008d73f
saif-qureshi <86108658+saif-qureshi@users.noreply.github.com>
2024-03-30 18:40:39 +0500

    Revert "Revert "Feature/comment listing""

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use App\Traits\HasApiWhere;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -55,10 +56,10 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function FullName() : Attribute
+    public function FullName(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->first_name. ' '. $this->last_name
+            get: fn () => $this->first_name . ' ' . $this->last_name
         );
     }
 
@@ -70,5 +71,10 @@ class User extends Authenticatable
     public function projects(): BelongsToMany
     {
         return $this->belongsToMany(Project::class, 'access_user_project');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\PhotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{
+    PhotoController,
+};
 
 /*
 |--------------------------------------------------------------------------

afcd43ce6e136249a2698ec32f136ee1f991265b
Saif Qureshi <mr.saq72437@gmail.com>
2024-06-08 20:31:35 +0500

    paused image capture cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('sync:photos')->everyFiveMinutes();
+        // $schedule->command('sync:photos')->everyFiveMinutes();
     }
 
     /**

de30ae760ec8b1ef8cebe2095c1852ce422823bf
Saif Qureshi <mr.saq72437@gmail.com>
2024-08-01 05:05:25 +0500

    fixed typo

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Eloquent\Builder;
 
 class Photo extends Model
 {

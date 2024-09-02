1473a64d474391d95df0ec5bd863a57504edfc89
Saif Qureshi <mr.saq72437@gmail.com>
2024-09-02 14:43:10 +0500

    removed header and sidebar from camera view

--- resources/views/components/admin/layout.blade.php
+++ resources/views/components/admin/layout.blade.php
@@ -11,52 +11,56 @@
 
 <body class="py-5 md:py-0">
     <x-admin.mobile-menu />
-    <div
-        class="top-bar-boxed h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] -mt-7 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
-        <div class="h-full flex items-center">
+    @if (!Route::is('camera.show'))
+        <div
+            class="top-bar-boxed h-[70px] md:h-[65px] z-[51] border-b border-white/[0.08] -mt-7 md:mt-0 -mx-3 sm:-mx-8 md:-mx-0 px-3 md:border-b-0 relative md:inset-x-0 md:top-0 sm:px-8 md:px-10 md:pt-10 md:bg-gradient-to-b md:from-slate-100 md:to-transparent dark:md:from-darkmode-700">
+            <div class="h-full flex items-center">
 
-            <a href="" class="logo -intro-x hidden md:flex">
-                <img alt="{{ config('app.name') }}" class="logo__image" src="{{ asset('dist/images/logo.png') }}">
-            </a>
+                <a href="" class="logo -intro-x hidden md:flex">
+                    <img alt="{{ config('app.name') }}" class="logo__image" src="{{ asset('dist/images/logo.png') }}">
+                </a>
 
-            <x-admin.breadcrumbs title="{{ $title }}" />
+                <x-admin.breadcrumbs title="{{ $title }}" />
 
-            <div class="intro-x dropdown w-8 h-8">
-                <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in scale-110"
-                    role="button" aria-expanded="false" data-tw-toggle="dropdown">
-                    <img alt="{{ auth()->user()->full_name }}"
-                        src="https://ui-avatars.com/api/?name={{ auth()->user()->full_name }}">
+                <div class="intro-x dropdown w-8 h-8">
+                    <div class="dropdown-toggle w-8 h-8 rounded-full overflow-hidden shadow-lg image-fit zoom-in scale-110"
+                        role="button" aria-expanded="false" data-tw-toggle="dropdown">
+                        <img alt="{{ auth()->user()->full_name }}"
+                            src="https://ui-avatars.com/api/?name={{ auth()->user()->full_name }}">
+                    </div>
+                    <div class="dropdown-menu w-56">
+                        <ul
+                            class="dropdown-content bg-primary/80 before:block before:absolute before:bg-black before:inset-0 before:rounded-md before:z-[-1] text-white">
+                            <li class="p-2">
+                                <div class="font-medium">
+                                    <a href="">
+                                        {{ auth()->user()->full_name }}
+                                    </a>
+                                </div>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider border-white/[0.08]">
+                            </li>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();document.getElementById('logout-form').submit();"
+                                    class="dropdown-item hover:bg-white/5">
+                                    <i data-lucide="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="dropdown-menu w-56">
-                    <ul
-                        class="dropdown-content bg-primary/80 before:block before:absolute before:bg-black before:inset-0 before:rounded-md before:z-[-1] text-white">
-                        <li class="p-2">
-                            <div class="font-medium">
-                                <a href="">
-                                    {{ auth()->user()->full_name }}
-                                </a>
-                            </div>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider border-white/[0.08]">
-                        </li>
-                        <li>
-                            <a href="{{ route('logout') }}"
-                                onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                                class="dropdown-item hover:bg-white/5">
-                                <i data-lucide="toggle-right" class="w-4 h-4 mr-2"></i> Logout </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    </ul>
-                </div>
+
             </div>
-
         </div>
-    </div>
+    @endif
     <div class="flex overflow-hidden">
-        <x-admin.sidebar></x-admin.sidebar>
+        @if (!Route::is('camera.show'))
+            <x-admin.sidebar></x-admin.sidebar>
+        @endif
         <div class="content" id="app">
             {{ $slot }}
         </div>
